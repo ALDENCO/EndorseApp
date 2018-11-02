ea56faf8bc04b324a6d30b3105c6159cb4b64c6d
Alexander Myers <booksbyalex@TONELOC.local>
2018-11-01 19:12:52 -0600

    Fix procfile to use gunicorn

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/endorseme' #this tells postgres to connect to my database entitled endorseme
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgres://localhost/endorseme') #this tells postgres to connect to my database entitled endorseme
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'secret key'
