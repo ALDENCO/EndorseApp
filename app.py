b0ca3e37203b886c3b3ef970ef1f1eb402777e03
Alexander Myers <booksbyalex@TONELOC.attlocal.net>
2019-01-03 14:01:04 -0700

    1.3.19 2PMCST updating heroku config var with api key via app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgres://fq
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'secret key'
+app.config['API_KEY'] = 'api'
 
 db = SQLAlchemy(app)
 
