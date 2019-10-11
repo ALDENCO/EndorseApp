cdcc77cb7ecba9b4dd2c79b999d7ce0925405187
Alexander Myers <alexmyers08@gmail.com>
2019-10-11 15:19:08 -0600

    flask migrate added for real this time?

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 import psycopg2
 from flask_migrate import Migrate
+migrate = Migrate(app, db)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
@@ -14,7 +15,7 @@ app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'secret key'
 app.config['API_KEY'] = os.getenv('API_KEY')
 app.debug = True
-migrate = Migrate(app, db)
+
 
 toolbar = DebugToolbarExtension(app)
 db = SQLAlchemy(app)
